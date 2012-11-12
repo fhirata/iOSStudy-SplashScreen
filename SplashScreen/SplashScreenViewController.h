62391265233983dbab054395e3ff34e10c6d7ad1
Fabio Hirata <fabio.hirata@gmail.com>
2012-11-11 22:26:07 -0800

    Setting Splash Screen and Icon.

--- SplashScreen/SplashScreenViewController.h
+++ SplashScreen/SplashScreenViewController.h
@@ -5,9 +5,28 @@
 //  Created by Fabio Hideki Hirata on 11/11/12.
 //  Copyright (c) 2012 Fabio Hideki Hirata. All rights reserved.
 //
+// Device icon
+//
+// iPhone/iPod touch without Retina display: 57x57 pixels
+// iPhone/iPod touch with Retina display: 114x114 pixels
+// iPad: 72x72 pixels
+//
+// Device splashscreen
+//
+// iPhone/iPod touc without Retina display: 320x480 pixels
+// iPhone/iPod touch with Retina display: 640x960 pixels
+// iPad: 1024x768 pixels
+//
+// Notes:
+// -Take larger size and set it as the height/width above, fill the rest with the background color
+// -Need to copy the images into the Project folders
+// -Splash screen should not use company logo, otherwise splash screen will flash faster and faster
+// with newer devices and it will seem like a bug.
 
 #import <UIKit/UIKit.h>
 
 @interface SplashScreenViewController : UIViewController
+
+-(IBAction)exitSplashScreen:(id)sender;
 
 @end
--- SplashScreen/SplashScreenViewController.m
+++ SplashScreen/SplashScreenViewController.m
@@ -14,16 +14,9 @@
 
 @implementation SplashScreenViewController
 
-- (void)viewDidLoad
+-(IBAction)exitSplashScreen:(id)sender
 {
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    exit(EXIT_SUCCESS);
 }
 
 @end
