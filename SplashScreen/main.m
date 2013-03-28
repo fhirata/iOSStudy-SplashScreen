2e14ad0792ff41e26f50a683eda74cd4f9fa82ee
Fabio Hirata <fabio.hirata@gmail.com>
2013-03-27 22:22:17 -0700

    New comment to test things.

--- SplashScreen/main.m
+++ SplashScreen/main.m
@@ -1,6 +1,7 @@
 //
 //  main.m
 //  SplashScreen
+//  This is a new comment in the splash main.m
 //
 //  Created by Fabio Hideki Hirata on 11/11/12.
 //  Copyright (c) 2012 Fabio Hideki Hirata. All rights reserved.
